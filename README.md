0604f52d40daabfd29880fe89d45d553257bf99f
BrianJVarley <brianvarley9@gmail.com>
2020-03-11 17:10:48 +0000

    move component service depdendency

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # Pluralsight's Unit Testing in Angular Course
-This course is up to date.
 
-To get started, clone the repo or download it
+## Development server
 
-npm install
-npm test
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Code scaffolding
+
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+## Running unit tests
+
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
 
 
 
---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,33 @@
+import { HeroesComponent } from './heroes.component';
+import { of } from 'rxjs/internal/observable/of';
+
+describe('HeroesComponent', () => {
+  let component: HeroesComponent;
+  let HEROES;
+  let mockHeroService;
+
+  beforeEach(() => {
+    HEROES = [
+      { ID: 1, NAME: 'SpiderSkan', strength: 8 },
+      { ID: 1, NAME: 'Corona Woman', strength: 8 },
+      { ID: 1, NAME: 'SuperDude', strength: 8 }
+    ];
+
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHeroe', 'deleteHero']);
+
+    component = new HeroesComponent(mockHeroService);
+  });
+
+  describe('delete', () => {
+    it('should remove the given item from the heroes list', () => {
+      mockHeroService.deleteHero.and.returnValue(of(true));
+      component.heroes = HEROES;
+
+      component.delete(HEROES[2]);
+
+      expect(component.heroes.length).toBe(2);
+
+
+    });
+  });
+});
