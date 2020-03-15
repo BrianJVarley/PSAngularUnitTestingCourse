83988fa82bfa805d5d5894ace9123a600d532d82
BrianJVarley <brianvarley9@gmail.com>
2020-03-15 16:25:11 +0000

    mock the child app-hero component instead of using NO_ERRORS_SCHEMA schema

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeroesComponent } from './heroes.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, Input, Component } from '@angular/core';
 import { HeroService } from '../hero.service';
+// tslint:disable-next-line: import-blacklist
 import { of } from 'rxjs';
+import { Hero } from '../hero';
 
 describe('HeroesComponent (shallow tests)', () => {
   let fixture: ComponentFixture<HeroesComponent>;
   let mockHeroService;
   let HEROES;
   const HEROES_INIT_SIZE = 3;
+
+  // Example of mocking the child 'app-hero' component
+  // Alternative to using risky NO_ERRORS_SCHEMA
+  @Component({
+    selector: 'app-hero',
+    template: '<div></div>'
+  })
+  class MockHeroComponent {
+    @Input() hero: Hero;
+  }
 
   beforeEach(() => {
     mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHeroes', 'deleteHeroes']);
@@ -18,9 +30,10 @@ describe('HeroesComponent (shallow tests)', () => {
       { ID: 1, NAME: 'SuperDude', strength: 8 }
     ];
     TestBed.configureTestingModule({
-      declarations: [HeroesComponent],
-      providers: [{ provide: HeroService, useValue: mockHeroService }],
-      schemas: [NO_ERRORS_SCHEMA]
+      declarations: [HeroesComponent, MockHeroComponent],
+      providers: [{ provide: HeroService, useValue: mockHeroService }]
+      // NO_ERRORS_SCHEMA can hide problems in template markup
+      // schemas: [NO_ERRORS_SCHEMA]
     });
 
     fixture = TestBed.createComponent(HeroesComponent);
